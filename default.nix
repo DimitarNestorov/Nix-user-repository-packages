206ca579c0d8603fa721ce2b95d90b46fa4f5cce
Dimitar Nestorov <8790386+dimitarnestorov@users.noreply.github.com>
2024-12-26 18:14:54 +0200

    skint: init at 1.08

--- default.nix
+++ default.nix
@@ -1,10 +1,16 @@
 { pkgs ? import <nixpkgs> { } }:
-
-{
-	eclecticlight = pkgs.lib.recurseIntoAttrs (
+let
+	eclecticlightPkgs = pkgs.lib.recurseIntoAttrs (
 		pkgs.lib.packagesFromDirectoryRecursive {
 			inherit (pkgs) callPackage;
 			directory = ./pkgs/eclectic-light;
 		}
 	);
+in {
+	eclecticlight = eclecticlightPkgs // {
+		skint-m = eclecticlightPkgs.skint.overrideAttrs {
+			pname = "skint-m";
+			app = "SkintM.app"; 
+		};
+	};
 }
